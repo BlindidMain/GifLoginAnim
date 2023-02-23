314923b23c4a0c43b69e2d9d31645064c2a6e902
akardas16 <krdsabdllh53@gmail.com>
2023-02-23 17:27:27 +0100

    updated

---
+++ GifLoginAnim/src/main/java/com/akardas/gifloginanim/GifAnim.kt
@@ -0,0 +1,42 @@
+package com.akardas.gifloginanim
+
+
+import android.content.Context
+import android.graphics.drawable.AnimationDrawable
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+
+class GifAnim : LinearLayout {
+    private lateinit var rocketAnimation: AnimationDrawable
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
+        context, attrs, defStyle
+    ) {
+        initView(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(
+        context, attrs
+    ) {
+        initView(context)
+    }
+
+    constructor(context: Context) : super(context) {
+        initView(context)
+    }
+
+    private fun initView(context: Context) {
+        inflate(context, R.layout.gif_layout, this)
+
+        val image:ImageView = rootView.findViewById<ImageView>(R.id.myGif).apply {
+
+            setBackgroundResource(R.drawable.gif_login_animation)
+            rocketAnimation = background as AnimationDrawable
+        }
+
+
+        rocketAnimation.start()
+    }
+}
